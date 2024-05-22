a90a8a66a4a96b0e9681f8d2da7dba4d0bf07557
vebsberly <vebarrio@my.cspc.edu.ph>
2024-05-22 14:54:47 +0800

    add posts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('products', ProductsController::class);
